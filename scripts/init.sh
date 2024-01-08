57c14756184b89ff1f1401bc7b9ea893c17863d4
devpolatto <polatto.xcode.job@gmail.com>
2024-01-07 21:10:56 -0300

    update

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
-echo "
-global:
-  scrape_interval:     15s # By default, scrape targets every 15 seconds.
+echo "global:
+  scrape_interval: 15s 
 scrape_configs:
   - job_name: 'prometheus'
     scrape_interval: 5s
@@ -12,7 +11,4 @@ scrape_configs:
   Example job for node_exporter
   - job_name: 'node_exporter'
     static_configs:
-      - targets: ['node_exporter:9100']
-" > /etc/prometheus/prometheus.yaml
-
-/bin/prometheus --config.file=/etc/prometheus/prometheus.yaml
+      - targets: ['node_exporter:9100']" > /etc/prometheus/prometheus.yaml
