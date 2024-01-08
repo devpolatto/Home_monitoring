599bbd2248e8b672c740c91d218721a8e705a4cb
devpolatto <polatto.xcode.job@gmail.com>
2024-01-07 21:18:08 -0300

    update

---
+++ init.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+encode=$(cat prometheus.yaml | base64)
+
+echo $encode
+
+# cat <<EOL > /etc/prometheus/prometheus.yaml
+# global:
+#   scrape_interval: 15s 
+
+# scrape_configs:
+#   - job_name: 'prometheus'
+#     scrape_interval: 5s
+#     static_configs:
+#       - targets: ['localhost:9090']
+
+#   - job_name: 'node_exporter'
+#     static_configs:
+#       - targets: ['node_exporter:9100']
+# EOL
